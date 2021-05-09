7c8365babfd82e4dc9561fe5656721f87e542057
Kiborg <yury_kiborg@bk.ru>
2021-05-09 20:51:07 +0300

    work with string
    string js

---
+++ lern.js
@@ -0,0 +1,14 @@
+let str = "Привет, Мир!";
+alert(str);
+
+let str1 = 'Привет,';
+let str2 = ' Мир!';
+alert(str1 + str2);
+
+let p = 'Hello, ';
+let name = 'Yury!';
+alert(p + name);
+
+let age = "Age is 35 years";
+let name2 = "Yura's ";
+alert(name2 + age);
